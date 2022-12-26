501bab6b857216e66a0e1daac0fea3edfc058f2f
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2022-12-26 23:07:03 +0100

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -26,8 +26,7 @@ async def root_route_handler(_):
         {
             "server_status": "running",
             "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
-            "connected_bots": len(multi_clients),
+            
             "loads": dict(
                 ("bot" + str(c + 1), l)
                 for c, (_, l) in enumerate(
